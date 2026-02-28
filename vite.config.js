efb4077a49e2f72b23c93ce288b265c4ce60ff76
Md Ashraf Ullah <96839511+MdAshrafUllah@users.noreply.github.com>
2026-02-28 13:16:49 +0600

    - fix error

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: "/Flutter-portfolio/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        404: resolve(__dirname, "index.html"),
+      },
+    },
+  },
 });
